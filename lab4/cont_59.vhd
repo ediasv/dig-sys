6560c3481e2d2c586079b863f2583c76962cfc1d
Eduardo Dias <142503604+ediasv@users.noreply.github.com>
2025-05-19 08:08:39 -0300

    feat: counters from 000-999 and 00-59

---
+++ lab4/cont_59.vhd
@@ -0,0 +1,44 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity cont_59 is
+  port (
+    i_clk : in std_logic;
+    o_clk : out std_logic;
+    segments_out : out std_logic_vector(13 downto 0)
+  );
+end entity;
+
+architecture behavioral of cont_59 is
+  component cont_9
+    port(
+      i_clk : in std_logic;
+      o_clk : out std_logic;
+      segments_out : out std_logic_vector(6 downto 0)
+    );
+  end component;
+
+  component cont_5
+    port(
+      i_clk : in std_logic;
+      o_clk : out std_logic;
+      segments_out : out std_logic_vector(6 downto 0)
+    );
+  end component;
+
+  signal internal_clk : std_logic := '0';
+begin
+  cont_5_inst: cont_5
+    port map(
+      i_clk => i_clk,
+      o_clk => internal_clk,
+      segments_out => segments_out(6 downto 0)
+    );
+
+  cont_9_inst : cont_9
+    port map (
+      i_clk => internal_clk,
+      o_clk => o_clk,
+      segments_out => segments_out(13 downto 7)
+    );
+end architecture behavioral;
--- lab4/cont_999.vhd
+++ lab4/cont_999.vhd
@@ -4,11 +4,41 @@ use ieee.std_logic_1164.all;
 entity cont_999 is
   port (
     i_clk : in std_logic;
-    o_clk : out std_logic
+    o_clk : out std_logic;
+    segments_out : out std_logic_vector(20 downto 0)
   );
 end entity;
 
 architecture behavioral of cont_999 is
-  signal internal_clk : std_logic;
+  component cont_9
+    port(
+      i_clk : in std_logic;
+      o_clk : out std_logic;
+      segments_out : out std_logic_vector(6 downto 0)
+    );
+  end component;
+
+  signal internal_clk_1 : std_logic := '0';
+  signal internal_clk_2 : std_logic := '0';
 begin
+  u0: cont_9
+    port map (
+      i_clk => i_clk,
+      o_clk => internal_clk_1,
+      segments_out => segments_out(6 downto 0)
+    );
+
+  u1: cont_9
+    port map (
+      i_clk => internal_clk_1,
+      o_clk => internal_clk_2,
+      segments_out => segments_out(13 downto 7)
+    );
+
+  u2: cont_9
+    port map (
+      i_clk => internal_clk_2,
+      o_clk => o_clk,
+      segments_out => segments_out(20 downto 14)
+    );
 end architecture behavioral;
