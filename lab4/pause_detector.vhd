a22e1a02bd48992ae3763cb273a3adcbbb59eb6a
Eduardo Dias <142503604+ediasv@users.noreply.github.com>
2025-05-20 19:57:48 -0300

    murilow

--- lab4/pause_detector.vhd
+++ lab4/pause_detector.vhd
@@ -14,6 +14,24 @@ entity pause_detector is
 end entity;
 
 architecture behavioral of pause_detector is
+  signal pause_internal : std_logic := '0';
 begin
 -- pause = '1' se o timer for igual a 250264
+  process(ms_unit, ms_tens, ms_hundreds, sec_unit, sec_tens, min)
+  begin
+    if (
+      ms_unit = "0100" and
+      ms_tens = "0110" and
+      ms_hundreds = "0010" and
+      sec_unit = "0000" and
+      sec_tens = "0101" and
+      min = "0010"
+    ) then
+      pause <= '1';
+    else
+      pause <= '0';
+    end if;
+  end process;
+
+  pause <= pause_internal;
 end architecture behavioral;
