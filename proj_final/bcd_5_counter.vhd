88705c9740f5df196b85ea53ca793bddb3b01240
Eduardo Dias <142503604+ediasv@users.noreply.github.com>
2025-06-30 23:44:43 -0300

    wip

--- proj_final/bcd_5_counter.vhd
+++ proj_final/bcd_5_counter.vhd
@@ -2,18 +2,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity bcd_9_counter is
+entity bcd_5_counter is
     port (
-        clk       : in  std_logic;
-        reset_n   : in  std_logic;
-        tick_out  : out std_logic;
-        count_out : out std_logic_vector(3 downto 0)
+        clk       : in  std_logic;                  
+        reset_n   : in  std_logic;                  
+        tick_out  : out std_logic;                  
+        count_out : out std_logic_vector(2 downto 0)
     );
-end entity bcd_9_counter;
+end entity bcd_5_counter;
 
-architecture rtl of bcd_9_counter is
+architecture rtl of bcd_5_counter is
 
-    signal count_reg : unsigned(3 downto 0);
+    signal count_reg : unsigned(2 downto 0);
 
 begin
 
@@ -22,7 +22,7 @@ begin
         if reset_n = '0' then
             count_reg <= (others => '0');
         elsif rising_edge(clk) then
-            if count_reg = 9 then
+            if count_reg = 5 then
                 count_reg <= (others => '0');
             else
                 count_reg <= count_reg + 1;
@@ -31,7 +31,7 @@ begin
     end process;
 
     count_out <= std_logic_vector(count_reg);
-
-    tick_out <= '1' when count_reg = 9 else '0';
+    
+    tick_out <= '1' when count_reg = 5 else '0';
 
 end architecture rtl;
--- proj_final/bcd_9_counter_desc.vhd
+++ proj_final/bcd_9_counter_desc.vhd
@@ -2,16 +2,16 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity bcd_9_counter is
+entity bcd_9_counter_desc is
     port (
         clk       : in  std_logic;
         reset_n   : in  std_logic;
         tick_out  : out std_logic;
         count_out : out std_logic_vector(3 downto 0)
     );
-end entity bcd_9_counter;
+end entity bcd_9_counter_desc;
 
-architecture rtl of bcd_9_counter is
+architecture rtl of bcd_9_counter_desc is
 
     signal count_reg : unsigned(3 downto 0);
 
@@ -20,18 +20,18 @@ begin
     process(clk, reset_n)
     begin
         if reset_n = '0' then
-            count_reg <= (others => '0');
+            count_reg <= to_unsigned(9, 4);
         elsif rising_edge(clk) then
-            if count_reg = 9 then
-                count_reg <= (others => '0');
+            if count_reg = 0 then
+                count_reg <= to_unsigned(9, 4);
             else
-                count_reg <= count_reg + 1;
+                count_reg <= count_reg - 1;
             end if;
         end if;
     end process;
 
     count_out <= std_logic_vector(count_reg);
 
-    tick_out <= '1' when count_reg = 9 else '0';
+    tick_out <= '1' when count_reg = 0 else '0';
 
 end architecture rtl;
